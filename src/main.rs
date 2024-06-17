8a426f195900bfb78368bf471d4acb624db013d5
Yuval Shavit <dev@yuvalshavit.com>
2024-06-16 21:27:51 -0400

    redoing Selectors
    Start from scratch, this time with good unit testing as we go.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use std::io;
 use std::io::{stdin, Read};
-use std::string::ToString;
+use std::{env, io};
 
 use crate::fmt_md::MdOptions;
 use crate::output::Stream;
+use crate::select::parse_selectors;
 use crate::tree::MdqNode;
 
 mod fmt_md;
 mod fmt_str;
 mod output;
+mod parsing_iter;
 mod select;
 mod str_utils;
 mod tree;
@@ -20,17 +21,17 @@ fn main() {
     stdin().read_to_string(&mut contents).expect("invalid input (not utf8)");
     let ast = markdown::to_mdast(&mut contents, &markdown::ParseOptions::gfm()).unwrap();
     let mdq: MdqNode = ast.try_into().unwrap();
-
     let mut out = output::Output::new(Stream(io::stdout()));
 
-    let selector = select::Selector::Heading(select::Matcher::Substring {
-        look_for: "Hello".to_string(),
-        anchored_left: true,
-        anchored_right: false,
-    });
+    let selectors_str = env::args().nth(1).unwrap_or("".to_string());
+    let selectors = parse_selectors(&selectors_str).expect("failed to parse selector");
 
-    let found = selector.find(&mdq);
+    let mut pipeline_nodes = vec![&mdq];
+    for selector in selectors {
+        let new_pipeline = selector.find_nodes(pipeline_nodes);
+        pipeline_nodes = new_pipeline;
+    }
 
-    fmt_md::write_md(&MdOptions::default(), &mut out, &found);
+    fmt_md::write_md(&MdOptions::default(), &mut out, &pipeline_nodes);
     out.write_str("\n");
 }
