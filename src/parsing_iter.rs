0d208cec9d4ad1a0c6c4ec0c1b6b3799bcab5d6b
Yuval Shavit <dev@yuvalshavit.com>
2024-06-16 21:21:04 -0400

    revamp of selectors

--- src/parsing_iter.rs
+++ src/parsing_iter.rs
@@ -1,7 +1,8 @@
 /// An iterator that ignores newlines and counts position within the input.
-struct ParsingIterator<I: Iterator<Item = char>> {
+pub struct ParsingIterator<I: Iterator<Item = char>> {
     iter: I,
     position: Position,
+    pending: Option<char>,
 }
 
 impl<I: Iterator<Item = char>> ParsingIterator<I> {
@@ -9,7 +10,64 @@ impl<I: Iterator<Item = char>> ParsingIterator<I> {
         Self {
             iter,
             position: Position { line: 1, column: 0 },
+            pending: None,
         }
+    }
+
+    pub fn input_position(&self) -> Position {
+        self.position
+    }
+
+    pub fn peek(&mut self) -> Option<char> {
+        if let pending @ Some(_) = self.pending {
+            return pending;
+        }
+        match self.iter.next() {
+            None => None,
+            peek @ Some(_) => {
+                self.pending = peek;
+                peek
+            }
+        }
+    }
+
+    pub fn drop_to_while<F>(&mut self, dropped_chars: &mut String, keep_dropping: F)
+    where
+        F: Fn(char) -> bool,
+    {
+        dropped_chars.clear();
+        loop {
+            match self.peek() {
+                None => return,
+                Some(ch) => {
+                    if keep_dropping(ch) {
+                        dropped_chars.push(ch);
+                        let _ = self.next();
+                    } else {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn drop_while<F>(&mut self, keep_dropping: F) -> String
+    where
+        F: Fn(char) -> bool,
+    {
+        let mut dropped_chars = String::with_capacity(8); // arbitrary guess
+        self.drop_to_while(&mut dropped_chars, keep_dropping);
+        dropped_chars
+    }
+
+    fn update_position(&mut self, ch: char) -> Option<char> {
+        if ch == '\n' {
+            self.position.line += 1;
+            self.position.column = 0;
+        } else {
+            self.position.column += 1;
+        }
+        Some(ch)
     }
 }
 
@@ -17,28 +75,23 @@ impl<I: Iterator<Item = char>> Iterator for ParsingIterator<I> {
     type Item = char;
 
     fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            match self.iter.next() {
-                None => return None,
-                Some('\n') => {
-                    self.position.line += 1;
-                    self.position.column = 0;
-                }
-                Some(ch) => {
-                    self.position.column += 1;
-                    return Some(ch);
-                }
-            }
+        if let Some(ch) = self.pending {
+            self.pending = None;
+            return self.update_position(ch);
+        };
+        match self.iter.next() {
+            None => None,
+            Some(ch) => self.update_position(ch),
         }
     }
 }
 
 #[derive(Debug, Copy, Clone, PartialEq)]
-struct Position {
+pub struct Position {
     /// 1-indexed line position
-    line: usize,
+    pub line: usize,
     /// 1-indexed char column position within the line.
-    column: usize,
+    pub column: usize,
 }
 
 #[cfg(test)]
@@ -47,17 +100,16 @@ mod tests {
 
     #[test]
     fn basic() {
-        let mut iter = ParsingIterator::new("ABC\nD\n\nEF".chars());
+        let mut iter = ParsingIterator::new("AB\nC".chars());
         assert_eq!(iter.position, Position { line: 1, column: 0 });
         next_and_check(&mut iter, Some('A'), Position { line: 1, column: 1 });
         next_and_check(&mut iter, Some('B'), Position { line: 1, column: 2 });
-        next_and_check(&mut iter, Some('C'), Position { line: 1, column: 3 });
-        next_and_check(&mut iter, Some('D'), Position { line: 2, column: 1 });
-        next_and_check(&mut iter, Some('E'), Position { line: 4, column: 1 });
-        next_and_check(&mut iter, Some('F'), Position { line: 4, column: 2 });
-        next_and_check(&mut iter, None, Position { line: 4, column: 2 });
+        next_and_check(&mut iter, Some('\n'), Position { line: 2, column: 0 });
+        next_and_check(&mut iter, Some('C'), Position { line: 2, column: 1 });
+
+        next_and_check(&mut iter, None, Position { line: 2, column: 1 });
         // and it's unchanged if I call it again
-        next_and_check(&mut iter, None, Position { line: 4, column: 2 });
+        next_and_check(&mut iter, None, Position { line: 2, column: 1 });
     }
 
     #[test]
@@ -67,18 +119,42 @@ mod tests {
     }
 
     #[test]
-    fn prefix_and_trailing_newlines() {
-        let mut iter = ParsingIterator::new("\nA\n\n".chars());
-        next_and_check(&mut iter, Some('A'), Position { line: 2, column: 1 });
-        next_and_check(&mut iter, None, Position { line: 4, column: 0 });
-        next_and_check(&mut iter, None, Position { line: 4, column: 0 });
+    fn peek_after_read() {
+        let mut iter = ParsingIterator::new("AB".chars());
+        next_and_check(&mut iter, Some('A'), Position { line: 1, column: 1 });
+        peek_and_check(&mut iter, Some('B'), Position { line: 1, column: 1 }); // position unchanged!
+        next_and_check(&mut iter, Some('B'), Position { line: 1, column: 2 });
+        peek_and_check(&mut iter, None, Position { line: 1, column: 2 });
+        next_and_check(&mut iter, None, Position { line: 1, column: 2 });
+
+        // no-ops
+        peek_and_check(&mut iter, None, Position { line: 1, column: 2 });
+        peek_and_check(&mut iter, None, Position { line: 1, column: 2 });
+        next_and_check(&mut iter, None, Position { line: 1, column: 2 });
+        next_and_check(&mut iter, None, Position { line: 1, column: 2 });
     }
 
     #[test]
-    fn only_newlines() {
-        let mut iter = ParsingIterator::new("\n\n\n".chars());
-        next_and_check(&mut iter, None, Position { line: 4, column: 0 });
-        next_and_check(&mut iter, None, Position { line: 4, column: 0 });
+    fn peek_initial() {
+        let mut iter = ParsingIterator::new("A".chars());
+        peek_and_check(&mut iter, Some('A'), Position { line: 1, column: 0 });
+        next_and_check(&mut iter, Some('A'), Position { line: 1, column: 1 });
+    }
+
+    #[test]
+    fn drop_while() {
+        let mut iter = ParsingIterator::new("A \t B".chars());
+
+        assert_eq!("", iter.drop_while(|ch| ch.is_whitespace()));
+        next_and_check(&mut iter, Some('A'), Position { line: 1, column: 1 });
+
+        assert_eq!(" \t ", iter.drop_while(|ch| ch.is_whitespace()));
+        peek_and_check(&mut iter, Some('B'), Position { line: 1, column: 3 });
+        next_and_check(&mut iter, Some('B'), Position { line: 1, column: 4 });
+
+        assert_eq!("", iter.drop_while(|ch| ch.is_whitespace()));
+        peek_and_check(&mut iter, None, Position { line: 1, column: 4 });
+        next_and_check(&mut iter, None, Position { line: 1, column: 4 });
     }
 
     fn next_and_check<I>(iter: &mut ParsingIterator<I>, expect_ch: Option<char>, expect_pos: Position)
@@ -87,6 +163,15 @@ mod tests {
     {
         let next_item = iter.next();
         assert_eq!(next_item, expect_ch);
-        assert_eq!(iter.position, expect_pos);
+        assert_eq!(iter.input_position(), expect_pos);
+    }
+
+    fn peek_and_check<I>(iter: &mut ParsingIterator<I>, expect_ch: Option<char>, expect_pos: Position)
+    where
+        I: Iterator<Item = char>,
+    {
+        let next_item = iter.peek();
+        assert_eq!(next_item, expect_ch);
+        assert_eq!(iter.input_position(), expect_pos);
     }
 }
--- src/select.rs
+++ src/select.rs
@@ -1,148 +1,354 @@
 use crate::fmt_str::inlines_to_plain_string;
-use crate::tree::*;
+use crate::parsing_iter::{ParsingIterator, Position};
+use crate::tree::MdqNode;
+use regex::Regex;
 
-#[allow(dead_code)]
+#[derive(Debug, PartialEq)]
 pub enum Selector {
+    Any,
     // List(Matcher, ListType, Selected),  // should this be list or list item?
-    Image { text: Matcher, href: Matcher },
-    Link { text: Matcher, href: Matcher },
-    CodeBlock(Matcher),
-    Heading(Matcher),
+    // Image { text: Matcher, href: Matcher },
+    // Link { text: Matcher, href: Matcher },
+    // CodeBlock(Matcher),
+    Heading(HeadingSelector),
     // TODO I need an "any", or maybe just a "paragraph", or maybe both
     // TODO does it make sense to select on a block quote?
 }
 
-#[allow(dead_code)]
-pub enum ListType {
-    Ordered,
-    Unordered,
+impl Selector {
+    // TODO do I need the lifetime specifiers?
+    pub fn find_nodes<'a>(&self, nodes: Vec<&'a MdqNode>) -> Vec<&'a MdqNode> {
+        let mut result = Vec::with_capacity(8); // arbitrary guess
+        for node in nodes {
+            self.find_nodes_one(&mut result, node);
+        }
+        result
+    }
+
+    // TODO need better name -- here but also in all the other methods
+    pub fn find_nodes_one<'a>(&self, out: &mut Vec<&'a MdqNode>, node: &'a MdqNode) {
+        match (self, node) {
+            (Selector::Heading(selector), MdqNode::Header(header)) => {
+                let header_text = inlines_to_plain_string(&header.title);
+                if selector.matcher.matches(&header_text) {
+                    header.body.iter().for_each(|child| out.push(child));
+                }
+            }
+            (_, MdqNode::Root(root)) => {
+                for node in &root.body {
+                    self.find_nodes_one(out, node);
+                }
+            }
+            _ => {
+                // TODO better recursion
+            }
+        }
+    }
 }
 
-#[allow(dead_code)]
-pub type Selected = Option<bool>;
+#[derive(Debug, PartialEq)]
+pub struct HeadingSelector {
+    matcher: Matcher,
+}
 
-#[allow(dead_code)]
+#[derive(Debug)]
 pub enum Matcher {
     Any,
-    Substring {
-        look_for: String,
-        anchored_left: bool,
-        anchored_right: bool,
-    },
-    Regex(regex::Regex),
+    Substring(SubstringMatcher),
+    Regex(Regex),
+}
+
+impl PartialEq for Matcher {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Matcher::Any, Matcher::Any) => true,
+            (Matcher::Substring(lhs), Matcher::Substring(rhs)) => lhs == rhs,
+            (Matcher::Regex(lhs), Matcher::Regex(rhs)) => lhs.as_str() == rhs.as_str(),
+            _ => false,
+        }
+    }
 }
 
 impl Matcher {
-    fn matches(&self, text: &str) -> bool {
+    fn matches(&self, haystack: &str) -> bool {
         match self {
             Matcher::Any => true,
-            Matcher::Substring {
-                look_for,
-                anchored_left,
-                anchored_right,
-            } => {
-                // TODO we can do this more efficiently, but this is good for now.
-                // In the future, we could take an approach of comparing each char of look_for to
-                // the next char in the &strs list.
-                match (anchored_left, anchored_right) {
-                    (false, false) => text.contains(look_for),
-                    (true, false) => text.starts_with(look_for),
-                    (false, true) => text.ends_with(look_for),
-                    (true, true) => text.eq(look_for),
-                }
-            }
-            Matcher::Regex(pattern) => pattern.is_match(&text),
+            Matcher::Substring(SubstringMatcher { look_for }) => haystack.contains(look_for),
+            Matcher::Regex(re) => re.is_match(haystack),
         }
     }
 }
 
-impl Selector {
-    pub fn find<'a>(&'a self, node: &'a MdqNode) -> Vec<&MdqNode> {
-        enum SelectResult<'a> {
-            Found(Vec<&'a MdqNode>),
-            Recurse(&'a Vec<MdqNode>),
-            RecurseOwned(Vec<&'a MdqNode>),
-            None,
-        }
+#[derive(Debug, PartialEq)]
+pub struct SubstringMatcher {
+    pub look_for: String,
+}
 
-        let result = match node {
-            MdqNode::Root(Root { body }) => SelectResult::Recurse(body),
-            MdqNode::Header(Header { title, body, .. }) => {
-                if let Selector::Heading(matcher) = self {
-                    if matcher.matches(&inlines_to_plain_string(title)) {
-                        SelectResult::Found(body.iter().map(|elem| elem).collect())
-                    } else {
-                        SelectResult::Recurse(body)
-                    }
-                } else {
-                    SelectResult::Recurse(body)
-                }
-            }
-            MdqNode::Paragraph(Paragraph { .. }) => {
-                SelectResult::None // see TODO on Selector
-            }
-            MdqNode::BlockQuote(BlockQuote { body }) => SelectResult::Recurse(body),
-            MdqNode::List(List { starting_index, items }) => {
-                let _is_ordered = starting_index.is_some(); // TODO use in selected
-                SelectResult::RecurseOwned(
-                    items
-                        .iter()
-                        .flat_map(|li| {
-                            // TODO check selected
-                            self.find_in_children(&li.item)
-                        })
-                        .collect(),
-                )
-            }
-            MdqNode::Table(Table { .. }) => {
-                SelectResult::None // TODO need to recurse
-            }
-            MdqNode::ThematicBreak => {
-                SelectResult::None // can't be selected, doesn't have children
-            }
-            MdqNode::CodeBlock(CodeBlock { variant, value }) => {
-                let matched = match (self, variant) {
-                    (Selector::CodeBlock(matcher), CodeVariant::Code(_)) => matcher.matches(value),
-                    (_, _) => false,
-                };
-                if matched {
-                    SelectResult::Found(vec![node])
-                } else {
-                    SelectResult::None
-                }
-            }
-            MdqNode::Inline(inline) => {
-                let matched = match inline {
-                    Inline::Span { variant, .. } => {
-                        match (self, variant) {
-                            (_, _) => false, // TODO links, etc
-                        }
-                    }
-                    Inline::Text { .. } => false,
-                    Inline::Link { .. } => {
-                        false // TODO
-                    }
-                    Inline::Image { .. } => {
-                        false // TODO
-                    }
-                    Inline::Footnote { .. } => false,
-                };
-                if matched {
-                    SelectResult::Found(vec![node])
-                } else {
-                    SelectResult::None
-                }
-            }
-        };
-        match result {
-            SelectResult::Found(results) => results,
-            SelectResult::Recurse(children) => self.find_in_children(children),
-            SelectResult::RecurseOwned(children) => children.iter().flat_map(|elem| self.find(elem)).collect(),
-            SelectResult::None => Vec::new(),
+#[derive(Debug, Clone, PartialEq)]
+pub struct ParseError {
+    position: Position,
+    reason: ParseErrorReason,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ParseErrorReason {
+    UnexpectedCharacter(char),
+    UnexpectedEndOfInput,
+    InvalidSyntax(String),
+}
+
+pub fn parse_selectors(text: &str) -> core::result::Result<Vec<Selector>, ParseError> {
+    let mut iter = ParsingIterator::new(text.chars());
+    let mut selectors = Vec::with_capacity(5); // just a guess
+
+    loop {
+        iter.drop_while(|ch| ch.is_whitespace());
+        if iter.peek().is_none() {
+            break;
+        }
+        let selector = Parser::parse_selector(&mut iter).map_err(|reason| ParseError {
+            reason,
+            position: iter.input_position(),
+        })?;
+        selectors.push(selector);
+    }
+
+    Ok(selectors)
+}
+
+/// just a helper so that I don't have to keep repeating the `C: Iterator<Item = char>` bounds.
+/// TODO is there a better way to do that?
+/// TODO ooor, I could use this parser to maintain a String that I can use as a scratch space, which has an initial
+/// capacity enough for the original string.
+struct Parser<C>(std::marker::PhantomData<C>);
+type Result<T> = core::result::Result<T, ParseErrorReason>;
+
+impl<C> Parser<C>
+where
+    C: Iterator<Item = char>,
+{
+    fn parse_selector(chars: &mut ParsingIterator<C>) -> Result<Selector> {
+        chars.drop_while(|ch| ch.is_whitespace()); // should already be the case, but this is cheap and future-proof
+        match chars.next() {
+            None => Ok(Selector::Any), // unexpected, but future-proof
+            Some('#') => Self::parse_header(chars),
+
+            Some(other) => Err(ParseErrorReason::UnexpectedCharacter(other)),
         }
     }
 
-    fn find_in_children<'a>(&'a self, children: &'a Vec<MdqNode>) -> Vec<&MdqNode> {
-        children.iter().flat_map(|elem| self.find(elem)).collect()
+    fn parse_header(chars: &mut ParsingIterator<C>) -> Result<Selector> {
+        let matcher = Self::parse_matcher(chars)?;
+        Ok(Selector::Heading(HeadingSelector { matcher }))
+    }
+
+    fn parse_matcher(chars: &mut ParsingIterator<C>) -> Result<Matcher> {
+        chars.drop_while(|ch| ch.is_whitespace());
+        let peek_ch = match chars.peek() {
+            None => return Ok(Matcher::Any),
+            Some(ch) => ch,
+        };
+        if peek_ch.is_alphanumeric() {
+            return Ok(Self::parse_matcher_bare(chars));
+        }
+        let _ = chars.next(); // drop the char we peeked
+        match peek_ch {
+            '*' => Ok(Matcher::Any),
+            '/' => {
+                if matches!(chars.peek(), Some('/')) {
+                    // not a regex, but an empty matcher
+                    let _ = chars.next(); // drop the '/'
+                    Ok(Matcher::Any)
+                } else {
+                    Self::parse_regex_matcher(chars)
+                }
+            }
+            _ => Err(ParseErrorReason::UnexpectedCharacter(peek_ch)),
+        }
+    }
+
+    fn parse_matcher_bare(chars: &mut ParsingIterator<C>) -> Matcher {
+        let mut result = String::with_capacity(20); // just a guess
+        let mut dropped = String::with_capacity(8); // also a guess
+
+        loop {
+            chars.drop_to_while(&mut dropped, |ch| ch.is_whitespace());
+            let Some(ch) = chars.next() else {
+                break;
+            };
+            if ch == '/' && matches!(chars.peek(), Some('/')) {
+                chars.next(); // consume the second '/'
+                break;
+            }
+            result.push_str(&dropped);
+            result.push(ch);
+        }
+
+        Matcher::Substring(SubstringMatcher { look_for: result })
+    }
+
+    fn parse_regex_matcher(chars: &mut ParsingIterator<C>) -> Result<Matcher> {
+        let mut result = String::with_capacity(20); // just a guess
+
+        loop {
+            match chars.next() {
+                None => return Err(ParseErrorReason::UnexpectedEndOfInput),
+                Some('\\') => match chars.next() {
+                    None => return Err(ParseErrorReason::UnexpectedEndOfInput),
+                    Some('/') => result.push('/'),
+                    Some(escaped) => {
+                        result.push('\\');
+                        result.push(escaped);
+                    }
+                },
+                Some('/') => break,
+                Some(ch) => result.push(ch),
+            }
+        }
+
+        match Regex::new(&result) {
+            Ok(re) => Ok(Matcher::Regex(re)),
+            Err(err) => Err(ParseErrorReason::InvalidSyntax(err.to_string())),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::fmt::Debug;
+
+    mod parse_selector {
+        use super::*;
+
+        #[test]
+        fn header() {
+            parse_and_check(
+                "# foo",
+                Selector::Heading(HeadingSelector {
+                    matcher: Matcher::Substring(SubstringMatcher {
+                        look_for: "foo".to_string(),
+                    }),
+                }),
+                "",
+            );
+
+            parse_and_check("# ", Selector::Heading(HeadingSelector { matcher: Matcher::Any }), "");
+
+            parse_and_check(
+                "# // next",
+                Selector::Heading(HeadingSelector { matcher: Matcher::Any }),
+                " next",
+            );
+        }
+
+        fn parse_and_check(text: &str, expect: Selector, expect_remaining: &str) {
+            parse_and_check_mapped(text, expect, expect_remaining, |iter| Parser::parse_selector(iter))
+        }
+    }
+
+    mod parse_matcher {
+        use super::*;
+        use indoc::indoc;
+
+        #[test]
+        fn bareword() {
+            parse_and_check(
+                "hello",
+                Matcher::Substring(SubstringMatcher {
+                    look_for: "hello".to_string(),
+                }),
+                "",
+            );
+            parse_and_check(
+                "hello ",
+                Matcher::Substring(SubstringMatcher {
+                    look_for: "hello".to_string(),
+                }),
+                "",
+            );
+            parse_and_check(
+                "hello / goodbye",
+                Matcher::Substring(SubstringMatcher {
+                    look_for: "hello / goodbye".to_string(),
+                }),
+                "",
+            );
+            parse_and_check(
+                "hello// goodbye",
+                Matcher::Substring(SubstringMatcher {
+                    look_for: "hello".to_string(),
+                }),
+                " goodbye",
+            );
+            parse_and_check(
+                "hello // goodbye",
+                Matcher::Substring(SubstringMatcher {
+                    look_for: "hello".to_string(),
+                }),
+                " goodbye",
+            );
+        }
+
+        #[test]
+        fn regex() {
+            parse_and_check(r#"/foo/"#, Matcher::Regex(Regex::new("foo").unwrap()), "");
+            parse_and_check(r#"/foo /"#, Matcher::Regex(Regex::new("foo ").unwrap()), "");
+            parse_and_check(r#"/foo/bar"#, Matcher::Regex(Regex::new("foo").unwrap()), "bar");
+            parse_and_check(r#"//"#, Matcher::Any, ""); // NOT a regex!
+            parse_and_check(r#"/\d/"#, Matcher::Regex(Regex::new("\\d").unwrap()), "");
+            parse_and_check(r#"/fizz\/buzz/"#, Matcher::Regex(Regex::new("fizz/buzz").unwrap()), "");
+
+            expect_err(
+                r#"/unclosed"#,
+                ParseErrorReason::UnexpectedEndOfInput,
+                Position {
+                    line: 1,
+                    column: "/unclosed".len(),
+                },
+            );
+
+            expect_err(
+                r#"/(unclosed paren/"#,
+                ParseErrorReason::InvalidSyntax(
+                    indoc! {r#"
+                    regex parse error:
+                        (unclosed paren
+                        ^
+                    error: unclosed group"#}
+                    .to_string(),
+                ),
+                Position {
+                    line: 1,
+                    column: "/(unclosed paren/".len(),
+                },
+            );
+        }
+
+        fn parse_and_check(text: &str, expect: Matcher, expect_remaining: &str) {
+            parse_and_check_mapped(text, expect, expect_remaining, |iter| Parser::parse_matcher(iter))
+        }
+
+        fn expect_err(text: &str, expect: ParseErrorReason, at: Position) {
+            let mut iter = ParsingIterator::new(text.chars());
+            let err = Parser::parse_matcher(&mut iter).expect_err("expected to fail parsing");
+            assert_eq!(iter.input_position(), at);
+            assert_eq!(err, expect);
+        }
+    }
+
+    fn parse_and_check_mapped<E, F>(text: &str, expect: E, expect_remaining: &str, mapper: F)
+    where
+        E: PartialEq + Debug,
+        F: FnOnce(&mut ParsingIterator<std::str::Chars>) -> Result<E>,
+    {
+        let mut iter = ParsingIterator::new(text.chars());
+        let actual = mapper(&mut iter).expect("failure in the test itself");
+        assert_eq!(actual, expect);
+        let mut actual_remaining = String::new();
+        while let Some(ch) = iter.next() {
+            actual_remaining.push(ch);
+        }
+        assert_eq!(&actual_remaining, expect_remaining);
     }
 }
