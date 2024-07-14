cde92ca7304d15e633356649d66598d9fc7b49ff
Yuval Shavit <dev@yuvalshavit.com>
2024-07-14 00:41:33 -0400

    section selector selects the whole section
    Instead of only selecting the body, it selects the title as well. This
    also brings in a simplification to remove `fn pick(..)`, if we provide
    various `Into<MdElemRef>`s.
    
    Resolves #108.

--- src/select/base.rs
+++ src/select/base.rs
@@ -1,12 +1,11 @@
 use crate::tree_ref::MdElemRef;
 
-pub trait Selector<'a, I: Copy> {
+pub trait Selector<'a, I: Copy + Into<MdElemRef<'a>>> {
     fn matches(&self, item: I) -> bool;
-    fn pick(&self, item: I) -> MdElemRef<'a>;
 
     fn try_select(&self, item: I) -> Option<MdElemRef<'a>> {
         if self.matches(item) {
-            Some(self.pick(item))
+            Some(item.into())
         } else {
             None
         }
--- src/select/sel_section.rs
+++ src/select/sel_section.rs
@@ -3,7 +3,6 @@ use crate::parsing_iter::ParsingIterator;
 use crate::select::base::Selector;
 use crate::select::{ParseResult, SELECTOR_SEPARATOR};
 use crate::tree::Section;
-use crate::tree_ref::MdElemRef;
 
 #[derive(Debug, PartialEq)]
 pub struct SectionSelector {
@@ -20,9 +19,5 @@ impl SectionSelector {
 impl<'a> Selector<'a, &'a Section> for SectionSelector {
     fn matches(&self, section: &'a Section) -> bool {
         self.matcher.matches_inlines(&section.title)
-    }
-
-    fn pick(&self, item: &'a Section) -> MdElemRef<'a> {
-        MdElemRef::Doc(&item.body)
     }
 }
