28ccd47991ee85bcba5254bf6c4fff36be612385
Yuval Shavit <dev@yuvalshavit.com>
2024-07-19 01:59:14 -0400

    require whitespace after `>` and `#` selectors
    This lets us add modifiers to them later if we want, like `#3`? idk.
    
    See #120

--- src/select/sel_block_quote.rs
+++ src/select/sel_block_quote.rs
@@ -11,6 +11,7 @@ pub struct BlockQuoteSelector {
 
 impl BlockQuoteSelector {
     pub fn read(iter: &mut ParsingIterator) -> ParseResult<Self> {
+        iter.require_whitespace_or(SELECTOR_SEPARATOR, ">")?;
         let matcher = StringMatcher::read(iter, SELECTOR_SEPARATOR)?;
         Ok(Self { matcher })
     }
--- src/select/sel_section.rs
+++ src/select/sel_section.rs
@@ -11,6 +11,7 @@ pub struct SectionSelector {
 
 impl SectionSelector {
     pub fn read(iter: &mut ParsingIterator) -> ParseResult<Self> {
+        iter.require_whitespace_or(SELECTOR_SEPARATOR, "#")?;
         let matcher = StringMatcher::read(iter, SELECTOR_SEPARATOR)?;
         Ok(Self { matcher })
     }
