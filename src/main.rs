5f68192501bfc3e10877c2dc4fcee8f9976b3fee
Yuval Shavit <dev@yuvalshavit.com>
2025-04-01 00:12:30 -0400

    unify the two IO implementations
    Add stdout and Error handling to OsFacade. This lets us unify the two
    previous implementations (`run_stdio` and `run_in_memory`).
    
    In service of #213.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use clap::Parser;
 use mdq::cli::Cli;
-use mdq::{run_stdio, OsFacade};
+use mdq::{Error, OsFacade};
 use std::io;
-use std::io::{stdin, Read};
+use std::io::{stdin, stdout, Read};
 use std::process::ExitCode;
 
 struct RealOs;
@@ -17,12 +17,20 @@ impl OsFacade for RealOs {
     fn read_file(&self, path: &str) -> io::Result<String> {
         std::fs::read_to_string(path)
     }
+
+    fn get_stdout(&mut self) -> impl io::Write {
+        stdout().lock()
+    }
+
+    fn write_error(&mut self, err: Error) {
+        eprint!("{err}")
+    }
 }
 
 fn main() -> ExitCode {
     let cli = Cli::parse();
 
-    if run_stdio(&cli, RealOs) {
+    if mdq::run(&cli, &mut RealOs) {
         ExitCode::SUCCESS
     } else {
         ExitCode::FAILURE
