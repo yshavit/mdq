19bf9654e60a253c516bf6978629f22b93e07821
Yuval Shavit <dev@yuvalshavit.com>
2025-08-03 18:41:00 -0400

    add sub-test mods

--- src/select/sel_single_matcher.rs
+++ src/select/sel_single_matcher.rs
@@ -89,141 +89,157 @@ mod test {
     };
     use crate::select::{MatchReplace, Matcher, TrySelector};
 
-    #[test]
-    fn block_quote_selector_match_error() {
-        let block_quote_matcher = BlockQuoteMatcher {
-            text: MatchReplace {
-                matcher: Matcher::Text {
-                    case_sensitive: false,
-                    anchor_start: false,
-                    text: "test".to_string(),
-                    anchor_end: false,
-                },
-                replacement: Some("replacement".to_string()),
-            },
-        };
+    mod block_quote {
+        use super::*;
 
-        let block_quote = BlockQuote {
-            body: vec![MdElem::Paragraph(Paragraph {
-                body: vec![Inline::Text(Text {
-                    variant: TextVariant::Plain,
-                    value: "test content".to_string(),
+        #[test]
+        fn block_quote_selector_match_error() {
+            let block_quote_matcher = BlockQuoteMatcher {
+                text: MatchReplace {
+                    matcher: Matcher::Text {
+                        case_sensitive: false,
+                        anchor_start: false,
+                        text: "test".to_string(),
+                        anchor_end: false,
+                    },
+                    replacement: Some("replacement".to_string()),
+                },
+            };
+
+            let block_quote = BlockQuote {
+                body: vec![MdElem::Paragraph(Paragraph {
+                    body: vec![Inline::Text(Text {
+                        variant: TextVariant::Plain,
+                        value: "test content".to_string(),
+                    })],
                 })],
-            })],
-        };
+            };
 
-        let block_quote_selector = BlockQuoteSelector::from(block_quote_matcher);
+            let block_quote_selector = BlockQuoteSelector::from(block_quote_matcher);
 
-        assert_eq!(
-            block_quote_selector.matches(&block_quote),
-            Err(StringMatchError::NotSupported)
-        );
+            assert_eq!(
+                block_quote_selector.matches(&block_quote),
+                Err(StringMatchError::NotSupported)
+            );
 
-        assert_eq!(
-            block_quote_selector
-                .try_select(&MdContext::default(), block_quote)
-                .unwrap_err()
-                .to_string(),
-            "block quote selector does not support string replace"
-        );
+            assert_eq!(
+                block_quote_selector
+                    .try_select(&MdContext::default(), block_quote)
+                    .unwrap_err()
+                    .to_string(),
+                "block quote selector does not support string replace"
+            );
+        }
     }
 
-    #[test]
-    fn paragraph_selector_match_error() {
-        let paragraph_matcher = ParagraphMatcher {
-            text: MatchReplace {
-                matcher: Matcher::Text {
-                    case_sensitive: false,
-                    anchor_start: false,
-                    text: "test".to_string(),
-                    anchor_end: false,
+    mod paragraph {
+        use super::*;
+
+        #[test]
+        fn paragraph_selector_match_error() {
+            let paragraph_matcher = ParagraphMatcher {
+                text: MatchReplace {
+                    matcher: Matcher::Text {
+                        case_sensitive: false,
+                        anchor_start: false,
+                        text: "test".to_string(),
+                        anchor_end: false,
+                    },
+                    replacement: Some("replacement".to_string()),
                 },
-                replacement: Some("replacement".to_string()),
-            },
-        };
+            };
 
-        let paragraph = Paragraph { body: vec![] };
+            let paragraph = Paragraph { body: vec![] };
 
-        let paragraph_selector = ParagraphSelector::from(paragraph_matcher);
+            let paragraph_selector = ParagraphSelector::from(paragraph_matcher);
 
-        assert_eq!(
-            paragraph_selector.matches(&paragraph),
-            Err(StringMatchError::NotSupported)
-        );
+            assert_eq!(
+                paragraph_selector.matches(&paragraph),
+                Err(StringMatchError::NotSupported)
+            );
 
-        assert_eq!(
-            paragraph_selector
-                .try_select(&MdContext::default(), paragraph)
-                .unwrap_err()
-                .to_string(),
-            "paragraph selector does not support string replace"
-        );
+            assert_eq!(
+                paragraph_selector
+                    .try_select(&MdContext::default(), paragraph)
+                    .unwrap_err()
+                    .to_string(),
+                "paragraph selector does not support string replace"
+            );
+        }
     }
 
-    #[test]
-    fn html_selector_match_error() {
-        let html_matcher = HtmlMatcher {
-            html: MatchReplace {
-                matcher: Matcher::Text {
-                    case_sensitive: false,
-                    anchor_start: false,
-                    text: "div".to_string(),
-                    anchor_end: false,
+    mod html {
+        use super::*;
+
+        #[test]
+        fn html_selector_match_error() {
+            let html_matcher = HtmlMatcher {
+                html: MatchReplace {
+                    matcher: Matcher::Text {
+                        case_sensitive: false,
+                        anchor_start: false,
+                        text: "div".to_string(),
+                        anchor_end: false,
+                    },
+                    replacement: Some("replacement".to_string()),
                 },
-                replacement: Some("replacement".to_string()),
-            },
-        };
+            };
 
-        let block_html = BlockHtml {
-            value: "<div>content</div>".to_string(),
-        };
+            let block_html = BlockHtml {
+                value: "<div>content</div>".to_string(),
+            };
 
-        let html_selector = HtmlSelector::from(html_matcher);
+            let html_selector = HtmlSelector::from(html_matcher);
 
-        assert_eq!(html_selector.matches(&block_html), Err(StringMatchError::NotSupported));
+            assert_eq!(html_selector.matches(&block_html), Err(StringMatchError::NotSupported));
 
-        assert_eq!(
-            html_selector
-                .try_select(&MdContext::default(), block_html)
-                .unwrap_err()
-                .to_string(),
-            "html selector does not support string replace"
-        );
+            assert_eq!(
+                html_selector
+                    .try_select(&MdContext::default(), block_html)
+                    .unwrap_err()
+                    .to_string(),
+                "html selector does not support string replace"
+            );
+        }
     }
 
-    #[test]
-    fn front_matter_selector_match_error() {
-        let front_matter_matcher = FrontMatterMatcher {
-            variant: None,
-            text: MatchReplace {
-                matcher: Matcher::Text {
-                    case_sensitive: false,
-                    anchor_start: false,
-                    text: "title".to_string(),
-                    anchor_end: false,
+    mod front_matter {
+        use super::*;
+
+        #[test]
+        fn front_matter_selector_match_error() {
+            let front_matter_matcher = FrontMatterMatcher {
+                variant: None,
+                text: MatchReplace {
+                    matcher: Matcher::Text {
+                        case_sensitive: false,
+                        anchor_start: false,
+                        text: "title".to_string(),
+                        anchor_end: false,
+                    },
+                    replacement: Some("replacement".to_string()),
                 },
-                replacement: Some("replacement".to_string()),
-            },
-        };
+            };
 
-        let front_matter = FrontMatter {
-            variant: FrontMatterVariant::Yaml,
-            body: "title: test".to_string(),
-        };
+            let front_matter = FrontMatter {
+                variant: FrontMatterVariant::Yaml,
+                body: "title: test".to_string(),
+            };
 
-        let front_matter_selector = FrontMatterSelector::from(front_matter_matcher);
+            let front_matter_selector = FrontMatterSelector::from(front_matter_matcher);
 
-        assert_eq!(
-            front_matter_selector.matches(&front_matter),
-            Err(StringMatchError::NotSupported)
-        );
+            assert_eq!(
+                front_matter_selector.matches(&front_matter),
+                Err(StringMatchError::NotSupported)
+            );
 
-        assert_eq!(
-            front_matter_selector
-                .try_select(&MdContext::default(), front_matter)
-                .unwrap_err()
-                .to_string(),
-            "front matter selector does not support string replace"
-        );
+            assert_eq!(
+                front_matter_selector
+                    .try_select(&MdContext::default(), front_matter)
+                    .unwrap_err()
+                    .to_string(),
+                "front matter selector does not support string replace"
+            );
+        }
     }
 }
